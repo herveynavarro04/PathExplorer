174e9ae135e11718a7fb4081bcc69e57e275f918
Mauricio Lozano <maulozano02@gmail.com>
2025-04-24 11:00:57 -0600

    Aplicacion a proyectos con scroll solo en los proyect cards  y buenas practicas con Next (server side y datos en un json aparte)

--- client/src/app/(main)/Aplicacion/ProyectoCard.jsx
+++ client/src/app/(main)/Aplicacion/ProyectoCard.jsx
@@ -1,24 +1,22 @@
-// src/components/ProyectoCard.jsx
 "use client";
+
 import React from "react";
 
 export default function ProyectoCard({ proyecto, onVerMas }) {
   return (
-    <div className="rounded-xl p-6 shadow-md bg-gradient-to-tl from-[#4C49ED] to-[#6F1DAA]">
-      <div className="w-[calc(100%+48px)] ml-[-24px] h-20 bg-gradient-to-t from-white/10 to-white/0 flex items-center px-6 justify-center">
+    <div className="rounded-xl p-6 shadow-md relative bg-gradient-to-tl from-[#4C49ED] to-[#6F1DAA]">
+      <div className="absolute top-0 left-0 w-full h-20 bg-gradient-to-t from-white/10 to-white/0 z-10 flex items-center px-6">
         <h2 className="text-white text-xl font-medium">{proyecto.title}</h2>
       </div>
 
-      <div className="relative w-full px-2 pt-5 text-[#d1c9f1]">
-        <p className="text-sm">{proyecto.resumen}</p>
-        <div className="mt-4">
-          <button
-            className="bg-[#3E0567] hover:bg-purple-800 text-white px-4 py-2 rounded"
-            onClick={onVerMas}
-          >
-            Ver más
-          </button>
-        </div>
+      <div className="relative w-full h-full px-2 py-0 pt-21">
+        <p className="mb-4 text-[#d1c9f1] text-lg">{proyecto.resumen}</p>
+        <button
+          className="bg-[#3E0567] hover:bg-purple-800 text-white px-4 py-2 rounded"
+          onClick={onVerMas}
+        >
+          Ver más
+        </button>
       </div>
     </div>
   );
