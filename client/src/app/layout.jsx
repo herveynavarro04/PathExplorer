3cfd1304ee6b98663a4486c4de5dce9e4f220620
sofia <sofiaschneiderj02@gmail.com>
2025-04-25 00:04:46 -0600

    font y colores

--- client/src/app/layout.jsx
+++ client/src/app/layout.jsx
@@ -1,15 +1,15 @@
 import { Geist, Geist_Mono } from "next/font/google";
 import "@/styles/globals.css";
 
-// const geistSans = Geist({
-//   variable: "--font-geist-sans",
-//   subsets: ["latin"],
-// });
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
 
-// const geistMono = Geist_Mono({
-//   variable: "--font-geist-mono",
-//   subsets: ["latin"],
-// });
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
 
 export const metadata = {
   title: "Path Explorer",
@@ -18,8 +18,8 @@ export const metadata = {
 
 export default function RootLayout({ children }) {
   return (
-    <html lang="en">
-      <body>{children}</body>
+    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
+      <body className="font-sans">{children}</body>
     </html>
   );
 }
