3c3645fde5e4c6e24e7035bff6ec500274634564
herveynavarro04 <herveynavarro2004@gmail.com>
2025-05-19 14:56:01 -0600

    minor fixs

--- server/src/certificates/certificates.module.ts
+++ server/src/certificates/certificates.module.ts
@@ -1,12 +1,12 @@
-import { Module } from '@nestjs/common';
-import { CertificatesService } from './certificates.service';
-import { CertificatesController } from './certificates.controller';
-import { TypeOrmModule } from '@nestjs/typeorm';
-import { CertificatesEntity } from './entities/certificates.entity';
+// import { Module } from '@nestjs/common';
+// import { CertificatesService } from './certificates.service';
+// import { CertificatesController } from './certificates.controller';
+// import { TypeOrmModule } from '@nestjs/typeorm';
+// import { CertificatesEntity } from './entities/certificates.entity';
 
-@Module({
-  imports: [TypeOrmModule.forFeature([CertificatesEntity])],
-  providers: [CertificatesService],
-  controllers: [CertificatesController],
-})
-export class CertificatesModule {}
+// @Module({
+//   imports: [TypeOrmModule.forFeature([CertificatesEntity])],
+//   providers: [CertificatesService],
+//   controllers: [CertificatesController],
+// })
+// export class CertificatesModule {}
