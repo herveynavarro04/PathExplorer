b0ea1405edd9da81759f8beddce111698ad53044
herveynavarro04 <herveynavarro2004@gmail.com>
2025-05-22 15:47:49 -0600

    final fix

--- server/src/employee/employee.controller.ts
+++ server/src/employee/employee.controller.ts
@@ -10,14 +10,14 @@ import {
   UseGuards,
   UseInterceptors,
 } from '@nestjs/common';
-import { JwtGuard } from 'src/common/Guards/jwt.guards';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { Request } from 'express';
-import { GetEmployeeInfoResponseDto } from './dto/response/getEmployeeInfo.response.dto';
+import { JwtGuard } from 'src/common/Guards/jwt.guards';
 import { UpdateEmployeeRequestDto } from './dto/request/updateEmployee.request.dto';
 import { DeleteEmployeeResponseDto } from './dto/response/deleteEmployee.response.dto';
+import { GetEmployeeInfoResponseDto } from './dto/response/getEmployeeInfo.response.dto';
 import { UpdateEmployeeResponseDto } from './dto/response/updateEmployee.response.dto';
 import { EmployeeService } from './employee.service';
-import { FileInterceptor } from '@nestjs/platform-express';
 
 @Controller('employee')
 export class EmployeeController {
@@ -40,6 +40,13 @@ export class EmployeeController {
   ): Promise<UpdateEmployeeResponseDto> {
     const employeeId = req.user['employeeId'];
     return this.employeeService.updateEmployee(employeeId, updatePayload, file);
+  }
+
+  @Patch('delete/picture')
+  @UseGuards(JwtGuard)
+  async deleteProfilePicture(@Req() req: Request): Promise<void> {
+    const employeeId = req.user['employeeId'];
+    return this.employeeService.deleteProfilePicture(employeeId);
   }
 
   @Delete(':employeeId')
