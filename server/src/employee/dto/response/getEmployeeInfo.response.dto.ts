e60046d4cedeac91e26b5d49154b1dad880f6add
herveynavarro04 <herveynavarro2004@gmail.com>
2025-05-22 14:38:12 -0600

    final fix

--- server/src/employee/dto/response/getEmployeeInfo.response.dto.ts
+++ server/src/employee/dto/response/getEmployeeInfo.response.dto.ts
@@ -1,4 +1,4 @@
-import { IsEmail, IsNotEmpty, IsString } from 'class-validator';
+import { IsEmail, IsNotEmpty, IsOptional, IsString } from 'class-validator';
 
 export class GetEmployeeInfoResponseDto {
   @IsEmail()
@@ -16,4 +16,8 @@ export class GetEmployeeInfoResponseDto {
   @IsString()
   @IsNotEmpty()
   profilePicture: Buffer | string;
+
+  @IsString()
+  @IsOptional()
+  mimeType?: string;
 }
