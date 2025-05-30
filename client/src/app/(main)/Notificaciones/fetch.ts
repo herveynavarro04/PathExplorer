d616e73de48f34904dbd02ce166b749531298eda
katiamvaldes <katiamcv04@gmail.com>
2025-05-30 16:17:30 -0600

    Notificaciones people lead

---
+++ client/src/app/(main)/Notificaciones/fetch.ts
@@ -0,0 +1,22 @@
+
+export async function getInfoNotification() {
+
+    return [
+      {
+        empleado: "Emilio Domínguez",
+        tema: "Meta Profesional",
+        descripcion: "Aprender a manejar Next.js",
+      },
+      {
+        empleado: "Nicolás Treviño",
+        tema: "Retroalimentación",
+        descripcion: "Es eficiente pero no aprovecha sus horas de trabajo",
+      },
+      {
+        empleado: "Carlos Martínez",
+        tema: "Certificado",
+        descripcion: "Certificado en Scrum Master",
+      },
+    ];
+  }
+  
