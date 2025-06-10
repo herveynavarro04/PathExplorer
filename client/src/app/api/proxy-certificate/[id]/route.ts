8296211495f5a261015a3efa046bfea9c50a9a6e
Sergio <sergio.vargas.contact@gmail.com>
2025-06-10 17:20:25 -0600

    urlFix

--- client/src/app/api/proxy-certificate/[id]/route.ts
+++ client/src/app/api/proxy-certificate/[id]/route.ts
@@ -1,38 +1,43 @@
 import { NextRequest, NextResponse } from "next/server";
-export async function GET(req: NextRequest, context: { params: { id: string } }) {
-    const certificateId = context.params.id;
-  
-    const authHeader = req.headers.get("authorization");
-    const token = authHeader?.split(" ")[1] || req.cookies.get("token")?.value;
+export async function GET(
+  req: NextRequest,
+  context: { params: { id: string } }
+) {
+  const certificateId = context.params.id;
 
-  
-    if (!token) {
-      return new NextResponse(JSON.stringify({ error: "Unauthorized" }), { status: 401 });
-    }
-  
-    const backendUrl = `http://localhost:8080/api/certificates/${certificateId}/file`;
-  
-    const backendRes = await fetch(backendUrl, {
-      headers: { Authorization: `Bearer ${token}` },
-    });
-  
-    if (!backendRes.ok) {
-      return new NextResponse(await backendRes.text(), {
-        status: backendRes.status,
-      });
-    }
-  
-    const contentType = backendRes.headers.get("Content-Type") || "application/octet-stream";
-    const contentDisposition = `inline; filename="preview.pdf"`;
-  
-    const fileBuffer = await backendRes.arrayBuffer();
-  
-    return new NextResponse(fileBuffer, {
-      status: 200,
-      headers: {
-        "Content-Type": contentType,
-        "Content-Disposition": `inline; filename="preview.pdf"`,
-      },
+  const authHeader = req.headers.get("authorization");
+  const token = authHeader?.split(" ")[1] || req.cookies.get("token")?.value;
+  const url = process.env.NEXT_PUBLIC_API_URL!;
+
+  if (!token) {
+    return new NextResponse(JSON.stringify({ error: "Unauthorized" }), {
+      status: 401,
     });
   }
-  
+
+  const backendUrl = `${url}/certificates/${certificateId}/file`;
+
+  const backendRes = await fetch(backendUrl, {
+    headers: { Authorization: `Bearer ${token}` },
+  });
+
+  if (!backendRes.ok) {
+    return new NextResponse(await backendRes.text(), {
+      status: backendRes.status,
+    });
+  }
+
+  const contentType =
+    backendRes.headers.get("Content-Type") || "application/octet-stream";
+  const contentDisposition = `inline; filename="preview.pdf"`;
+
+  const fileBuffer = await backendRes.arrayBuffer();
+
+  return new NextResponse(fileBuffer, {
+    status: 200,
+    headers: {
+      "Content-Type": contentType,
+      "Content-Disposition": `inline; filename="preview.pdf"`,
+    },
+  });
+}
